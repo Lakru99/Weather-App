80c7951a5dd3160746d714b2cadc0fd479199dc0
Lakru99 <lakru999@gmail.com>
2024-09-06 23:27:39 +0530

    ad new features like sun rise and sut set

--- assests/js/app.js
+++ assests/js/app.js
@@ -6,7 +6,13 @@ let img = document.getElementById("weatherIcon");
 let localtimeElement = $(".time");
 let wind_kph = $(".windSpeedP");
 let conditionText = $(".weatherCondition");
+let province = $(".provinceP");
 
+let sunRise = $(".sunriseP");
+let sunSet = $(".sunsetP");
+
+let humidity = $(".humidityP");
+let windDirection = $(".windDirec");
 
 const apiKey = "a66640216bab455b99a50251240609";
 
@@ -21,8 +27,9 @@ document.getElementById("search-button").addEventListener("click", handleSearch)
 function fetchData(location) {
     $.ajax({
       method: "GET",
-      url: `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
-      success: ({ location, current }) => {
+      // url: `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`
+      url: `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1`,
+      success: ({ location, current , forecast }) => {
         countryP.text(location.country);
         idP.text(current.temp_c.toLocaleString(undefined,{style:"unit",unit:"celsius"}));
         nameP.text(location.name);
@@ -44,6 +51,16 @@ function fetchData(location) {
         wind_kph.text(current.wind_kph + "kph");
  
         conditionText.text(current.condition.text);
+
+        province.text(location.region ? ` ${location.region}` : " ");
+        
+        // sunrise and sunset
+        const astro = forecast.forecastday[0].astro;
+        sunRise.text(`${astro.sunrise}`);
+        sunSet.text(`${astro.sunset}`);
+
+        humidity.text(current.humidity);
+        windDirection.text(current.wind_dir);
     }
   });
 }
